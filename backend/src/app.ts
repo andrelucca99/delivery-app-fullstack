e2f334f4cb206a4584f78deff7bc66a9089be1c4
André Lucas <andrelucca99@gmail.com>
2026-02-07 09:35:40 -0300

    refactor: alterando a versão 7 do prisma para versão 6 para trabalhar com mysql mais compativel

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import { prisma } from "./database/prisma";
 
 export const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/health", (_req, res) => {
-  res.json({ ok: true });
+app.get("/health", async (_req, res) => {
+  const usersCount = await prisma.user.count();
+  res.json({ ok: true, users: usersCount });
 });
